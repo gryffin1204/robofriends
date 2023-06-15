244429fc4c81e57fb92e2e1dd16f2126209c7581
gryffin1204 <vaibhavgoswami15ko@gmail.com>
2023-06-15 22:06:26 +0530

    Updates

---
+++ precache-manifest.77626f786903ac51306e276c58e2d7bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fde7ccb78e5da1542c53",
+    "url": "/robofriends/static/css/main.77dc5230.chunk.css"
+  },
+  {
+    "revision": "fde7ccb78e5da1542c53",
+    "url": "/robofriends/static/js/main.fde7ccb7.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "e5e660e88269b37601f0",
+    "url": "/robofriends/static/css/2.0143bc4e.chunk.css"
+  },
+  {
+    "revision": "e5e660e88269b37601f0",
+    "url": "/robofriends/static/js/2.e5e660e8.chunk.js"
+  },
+  {
+    "revision": "0e09ba9c7502e90c21a0",
+    "url": "/robofriends/static/js/runtime~main.0e09ba9c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0af316b3e610b798ac8526ae8e61395e",
+    "url": "/robofriends/index.html"
+  }
+];
